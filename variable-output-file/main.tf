d8c7e78565444eda4ecf182aad178f639236a862
Ubuntu <ubuntu@ip-172-31-42-82.ec2.internal>
2021-01-12 06:22:24 +0000

    adding code

---
+++ variable-output-file/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.9.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+
+resource "docker_image" "nodered_image" {
+  name = "nodered/node-red-docker:v8"
+}
+
+resource "random_string" "random" {
+  count   = var.container_count
+  length  = 4
+  special = false
+  upper   = false
+}
+
+
+resource "docker_container" "nodered_contianer" {
+  count = var.container_count
+  name  = join("-", ["nodered", random_string.random[count.index].result])
+  image = docker_image.nodered_image.latest
+  ports {
+    internal = var.int_port
+    #external = var.ext_port
+  }
+}
+
+
+
